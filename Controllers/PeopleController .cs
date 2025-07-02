9e73d5f83fc3a5fad6a6ff5c4be06e33f50a01dc
sharathkumar-dl <sharusharath343@gmail.com>
2025-07-02 23:13:04 +0530

    Add project files.

---
+++ Controllers/PeopleController .cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test_dev_pagination.Models;
+
+namespace Test_dev_pagination.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // Dummy data
+        private static List<Person> DummyData = Enumerable.Range(1, 100).Select(i => new Person
+        {
+            Id = i,
+            Name = $"Person {i}"
+        }).ToList();
+
+        // Loads the page
+        public IActionResult People()
+        {
+            return View();
+        }
+
+        // Server-side pagination endpoint
+        [HttpGet]
+        public JsonResult LoadData(int page = 1, int pageSize = 10)
+        {
+            var total = DummyData.Count;
+            var data = DummyData.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            return Json(new
+            {
+                data = data,
+                total = total
+            });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test_dev_pagination.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
